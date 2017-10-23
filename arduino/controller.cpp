20400009fa5782990162511c922a84488947c82f
Marco Stellin <letal32@gmail.com>
2017-10-23 23:49:37 +0100

    Added last uploaded code

--- arduino/controller.cpp
+++ arduino/controller.cpp
@@ -14,9 +14,10 @@ static const float A3f=0.4128;
 #define a 1
 #define b 1
 #define T 0.01
+#define deadzone 10
 
-extern int sensorValue;                 // value read  from  the pot
-extern long int outputValue;                 // value output  to  the PWM (analog out)
+extern int sensorValue;                       // value read  from  the pot
+extern long int outputValue;                  // value output  to  the PWM (analog out)
 extern bool begin_filters;
 extern int counter_small;
 extern float LUX_mat[max_Output/n_increments+1][2];
@@ -33,7 +34,6 @@ float K1 = Kp*b;
 float K2 = Kp*Ki*T/2;
 float K3 = Kd/(Kd+a*T);
 float K4 = Kp*Kd*a/(Kd+a*T);
-float K_wind=0.5*K2;
 double ff;
 
 
@@ -47,75 +47,66 @@ int cool_counter;
 
 void PID(float ref)
 {
-double e;
-double p;
-double i;
-double d;
-double y;
-float windup=0;
+  double e;
+  double p;
+  double i;
+  double d;
+  double y;
 
+  sensorValue = analogRead(analogInPin); //  read  the analog  in  value 
 
-sensorValue = analogRead(analogInPin); //  read  the analog  in  value 
+  sensorValue=PWM2LUX(sensorValue);
 
+  filter_butter(&sensorValue_filt, &y1_s, &y2_s, sensorValue, &x1_s, &x2_s);
 
-sensorValue=PWM2LUX(sensorValue);
+  e = ref- sensorValue_filt;
+  p = K1*ref -Kp*y;
+  i = i_ant + K2*(e + e_ant);
+  d = K3*d_ant - K4*(y - y_ant);
 
-filter_butter(&sensorValue_filt, &y1_s, &y2_s, sensorValue, &x1_s, &x2_s);
-//FILTER
-//ANTYWIND
+  outputValue = (long int) (p+i+d+ff);
 
-e = ref- sensorValue_filt;
-p = K1*ref -Kp*y;
-i = i_ant + K2*(e + e_ant);
-d = K3*d_ant - K4*(y - y_ant);
+  //Saturator
+  if (outputValue>255) 
+  {
+    i=i_ant;
+    outputValue=255;
+  }
+  if (outputValue<0)
+  {
+    i=i_ant;
+    outputValue=0; 
+  }
 
-outputValue = (long int) (p+i+d+ff);
+  // Deadzone
+  if(outputValue<deadzone)
+    outputValue=0;
 
-//Saturator
-if (outputValue>255) 
-{
-    windup=255-outputValue;
-  outputValue=255;
+  analogWrite(analogOutPin, outputValue); 
 
-
-}
-if (outputValue<0)
-{
-  windup=255-outputValue;
-  outputValue=0;
-
+  i_ant = i;
+  y_ant = y;
   
-}
-
-analogWrite(analogOutPin, outputValue); 
-
-i_ant = i+windup;
-y_ant = y;
-
-d_ant = d;
-e_ant = e;
+  d_ant = d;
+  e_ant = e;
 
   cool_counter++;
   if (cool_counter==100)
   {
-            Serial.println("LUX:");
-        Serial.println(sensorValue_filt);
-        
-        Serial.println("output:");
-        Serial.println(outputValue);
+    Serial.println("LUX:");
+    Serial.println(sensorValue_filt);
+    
+    Serial.println("output:");
+    Serial.println(outputValue);
 
-        Serial.println("i:");
-        Serial.println(sensorValue_filt);
+    /*Serial.println("i:");
+    Serial.println(sensorValue_filt);
 
-        Serial.println("i:");
-        Serial.println(sensorValue);  
+    Serial.println("i:");
+    Serial.println(sensorValue); */ 
 
-        cool_counter=0;
+    cool_counter=0;
   }
-
-
-
-
 }
 
 
@@ -131,50 +122,44 @@ void filter_butter(float *y, float *y1, float *y2, float x, float *x1, float *x2
 
 void Controller (float ref_PWM)
 {
-int refOutput;
-if (counter_small==1)
-{
-
-
-	//FILTER INPUT
-	if (begin_filters==true)
-	{
-		    sensorValue_filt=0;
-    		y1_s=0;
-    		y2_s=0;
-    		x1_s=0;
-    		x2_s=0;
-		begin_filters=false;
-    
-LUX_mat[max_Output/n_increments+1][2];
-
-
-    for (int i=0; i < max_Output/n_increments; i++){
-      if (ref_PWM < LUX_mat[i][1]){
-        float x1 = LUX_mat[i-1][0];
-        float x2 = LUX_mat[i][0];
-        float y1 = LUX_mat[i-1][1];
-        float y2 = LUX_mat[i][1];
-
-        float m = (y2-y1)/(x2-x1);
-        float c = y1-m*x1;
-        ff = (ref_PWM - c)/m;
-      }
-    }
-        Serial.println("ffd:");
-        Serial.println(refOutput);
-
-	}
-
-
-
-	PID(ref_PWM);
-	counter_small=0;
-
+  int refOutput;
   
-}
+  if (counter_small==1)
+  {
+  	//FILTER INPUT
+  	if (begin_filters==true)
+  	{
+	    sensorValue_filt=0;
+  		y1_s=0;
+  		y2_s=0;
+  		x1_s=0;
+  		x2_s=0;
+		  begin_filters=false;
+    
+      LUX_mat[max_Output/n_increments+1][2];
 
+      for (int i=0; i < max_Output/n_increments; i++){
+        if (ref_PWM < LUX_mat[i][1]){
+          float x1 = LUX_mat[i-1][0];
+          float x2 = LUX_mat[i][0];
+          float y1 = LUX_mat[i-1][1];
+          float y2 = LUX_mat[i][1];
+  
+          float m = (y2-y1)/(x2-x1);
+          float c = y1-m*x1;
+          ff = (ref_PWM - c)/m;
+        }
+      }
+    
+      Serial.println("ffd:");
+      Serial.println(refOutput);
 
+	  }
+
+  	PID(ref_PWM);
+  	counter_small=0;
+
+  }
 }
 
 
--- arduino/main.ino
+++ arduino/main.ino
@@ -1,55 +1,88 @@
 #include "general.h"
+
 /****************************************************************************/
 /* Global variables                                                         */
 /****************************************************************************/
-int sensorValue;                 // value read  from  the pot
+
+int sensorValue;                      // value read  from  the pot
 long int outputValue;                 // value output  to  the PWM (analog out)
 
-int counter_small;  //counter for measurements 
+int counter_small;                    //counter for measurements 
 
 float LUX_mat[max_Output/n_increments+1][2];
 
-long int microseconds=10000;     //tempo interrupção
+long int microseconds=10000;          //interruption time
+bool begin_filters;               
 
-float ref_PWM=30;
+float ref_PWM=30;                     // Reference   
+char rx_byte = 0;
+String rx_str = "";
+boolean not_number = false;
 
-bool begin_filters;
-
-//interruption should not have alot of stuff inside
+/****************************************************************************/
+/* Time Interruption                                                        */
+/****************************************************************************/
 void tempo() 
 {
-	counter_small=1;
+  counter_small=1;
 }
-
-
 
 /****************************************************************************/
 /* Setup routine - runs once when reset is pressed                          */
 /****************************************************************************/
 void setup() 
 {
-  	// initialize serial communication at 9600 bits per second:
-	Serial.begin(38400);
-    // initialize interrupt
-	Timer1.initialize(microseconds) ;
-  	Timer1.attachInterrupt(tempo); //insert the function the interrupt goes to
-  	//Serial.println(outputValue); 
-  	//Serial.print("---\n"); 
-
-  	counter_small=0; 
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(38400);
   
-  	sensorValue = 0; 
-  	outputValue = 0; 
+  // initialize interrupt
+	Timer1.initialize(microseconds) ;
+  Timer1.attachInterrupt(tempo); 
+  
+	//Serial.println(outputValue); 
+	//Serial.print("---\n"); 
 
+	counter_small=0; 
 
-  	Calibration();
-  	Serial.println("Exited setup");
-  	begin_filters=true;
+	sensorValue = 0; 
+	outputValue = 0; 
 
+  Calibration();
+  Serial.println("Exited setup");
+  begin_filters=true;
 
 }
 
+/****************************************************************************/
+/* Loop routine - runs once when reset is pressed                          */
+/****************************************************************************/
 void loop() 
 {   
+  //To read imputs from the serial monitor!
+  if (Serial.available() > 0) {    // is a character available?
+    rx_byte = Serial.read();       // get the character
+    
+    if ((rx_byte >= '0') && (rx_byte <= '9')) {
+      rx_str += rx_byte;
+    }
+    else if (rx_byte == '\n') {
+      // end of string
+      if (not_number) {
+        Serial.println("Not a number");
+      }
+      else {
+        Serial.println("ref_PWM:");
+        Serial.println(ref_PWM);
+        ref_PWM = rx_str.toInt();
+      }
+      not_number = false;         // reset flag
+      rx_str = "";                // clear the string for reuse
+    }
+    else {
+      // non-number character received
+      not_number = true;    // flag a non-number
+    }
+  } 
+    
 	Controller(ref_PWM);
 }
